29459f8b18f5198c6b4e14da4e2a60d799a46855
Supansa <s6201012620236@email.kmutnb.ac.th>
2022-05-15 14:06:14 +0700

    can select method for root finding

--- src/components/finding-root/finding-root.js
+++ src/components/finding-root/finding-root.js
@@ -6,7 +6,7 @@ import { RootMethod } from "../../constants/root-equaltion.js";
 
 export default function FindingRootComponent() {
   const [root, setRoot] = useState();
-  const [methodSelect, setMethodSelect] = useState();
+  const [methodSelect, setMethodSelect] = useState(1);
   const { sol, timeSpent, changeSol, changeTimeSpent } = useContext(ResultContext)
   const { selectFunc } = useContext(SelectFunctionContext)
 
@@ -17,7 +17,8 @@ export default function FindingRootComponent() {
     });
   }, [methodSelect]);
 
-  const getResult = () => {
+  const getResult = (event) => {
+    setMethodSelect(event.target.value)
     let startTime = performance.now()
     changeSol(root(selectFunc))
     let endTime = performance.now()
@@ -26,9 +27,23 @@ export default function FindingRootComponent() {
 
   return(
     <React.Fragment>
+      <div className="row my-3 mx-1">
+        <p className="col-7 bg-lightgrey rounded-3 p-2 mb-0">function</p>
+        <div className="input-group col">
+          <label className="input-group-text" htmlFor="methodSelect">Method</label>
+          <select className="form-select" id="methodSelect" defaultValue='0' onChange={(event)=> getResult(event)}>
+            <option value="0">Choose...</option>
+            <option value="1">Bisection</option>
+            <option value="2">Newton</option>
+            <option value="3">Regula Falsi</option>
+            <option value="4">Secant</option>
+          </select>
+        </div>
+      </div>
+
       <div className="p-2">
-        <p>{ 'solution :' + sol }</p>
-        <p>{ 'time spent :' + timeSpent }</p>
+        <p>{ 'solution : ' + sol }</p>
+        <p>{ 'time spent : ' + timeSpent }</p>
       </div>
     </React.Fragment>
   )
