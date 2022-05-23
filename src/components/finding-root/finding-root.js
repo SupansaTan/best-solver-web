39c8815815214bb6a5b39d5d2be8d28ab55e253b
Supansa <s6201012620236@email.kmutnb.ac.th>
2022-05-23 19:41:21 +0700

    can select function on root finding page

--- src/components/finding-root/finding-root.js
+++ src/components/finding-root/finding-root.js
@@ -3,13 +3,14 @@ import createModule from "../../wasm/mjs/findingRoot.mjs";
 import { ResultContext } from "../../context/result.js";
 import { SelectFunctionContext } from "../../context/select-function.js";
 import { RootMethod, RootEquation } from "../../constants/root-equation.js";
+import { Equation } from "../../constants/equation.js";
 import MathJax from 'react-mathjax';
 
 export default function FindingRootComponent() {
   const [root, setRoot] = useState();
-  const [methodSelect, setMethodSelect] = useState(1);
+  const [methodSelect, setMethodSelect] = useState(0);
   const { sol, timeSpent, changeSol, changeTimeSpent } = useContext(ResultContext)
-  const { selectFunc } = useContext(SelectFunctionContext)
+  const { selectFunc, changeSelectFunc } = useContext(SelectFunctionContext)
 
   useEffect(
     () => {
@@ -19,19 +20,44 @@ export default function FindingRootComponent() {
   }, [methodSelect]);
 
   const getResult = () => {
-    let startTime = performance.now()
-    changeSol(root(selectFunc))
-    let endTime = performance.now()
-    changeTimeSpent(endTime-startTime)
+    if(selectFunc>0 && methodSelect>0) {
+      let startTime = performance.now()
+      changeSol(root(selectFunc))
+      let endTime = performance.now()
+      changeTimeSpent(endTime-startTime)
+    }
+    else {
+      changeSol('')
+      changeTimeSpent('')
+    }
+  }
+
+  const selectFunction = () => {
+    return (
+      <React.Fragment>
+        {
+          Equation.map((item, index) => {
+            return (
+              <option value={index+1} key={'equal' + index+1}>
+                { item }
+              </option>
+            )
+          })
+        }
+      </React.Fragment>
+    )
   }
 
   return(
     <React.Fragment>
-      <div className="row my-3 mx-1">
+      <div className="row my-3 px-1">
         {/* show function */}
-        <p className="col-7 bg-lightgrey rounded-3 p-2 mb-0">
-          <MathJax.Node inline formula={ RootEquation[selectFunc] } />
-        </p>
+        <div className="col-7 rounded-3">
+          <select className="form-select" id="funcSelect" defaultValue='0' onChange={(event)=> {changeSelectFunc(event.target.value);getResult() }}>
+            <option value="0">Choose Function...</option>
+            { selectFunction() }
+          </select>
+        </div>
 
         {/* select method for find solution */}
         <div className="input-group col">
---
+++ src/constants/equation.js
@@ -0,0 +1,12 @@
+export const Equation = [
+  `f(x) = e^{-x} -x`,
+  `f(x) = xe^{0.5x} + 1.2x -5`,
+  `f(x) = x^{3} - x - 1`,
+  `f(x) = x^{3} - x^{2} - 1`,
+  `f(x) = x^{2} - 2 `,
+  `f(x) = e^{-x^{2}} - x`,
+  `f(x) = -3.36x + 15.25`,
+  `f(x) = \left (\frac{1}{5}\right )^{x} - 2`,
+  `f(x) = x^{3} - 3x^{2} - 4x + 5`,
+  `f(x) = -\left (\frac{1}{3}\right )^{x} + 2`
+]
