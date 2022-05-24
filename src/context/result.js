48d6f66551334782ccaf020c24170fdb10127a2f
hathaipat-p <hathaipat44@hotmail.co.th>
2022-05-24 21:50:43 +0700

    add python sol in context/result.js

--- src/context/result.js
+++ src/context/result.js
@@ -12,12 +12,17 @@ export const ResultContext = React.createContext(contextDefault);
 export const ResultProvider = ({ children }) => {
   const [sol, setSol] = useState('')
   const [timeSpent, setTimeSpent] = useState('')
+  const [pysol, setPySol] = useState('')
+  const [pytimeSpent, setPyTimeSpent] = useState('')
 
   const changeSol = (solution) => setSol(solution);
   const changeTimeSpent = (time) => setTimeSpent(time)
+  const changePySol = (solution) => setPySol(solution);
+  const changePyTimeSpent = (time) => setPyTimeSpent(time)
 
   return(
-    <ResultContext.Provider value={{ sol, changeSol, timeSpent, changeTimeSpent }}>
+    <ResultContext.Provider value={{ sol, changeSol, timeSpent, changeTimeSpent, 
+                                    pysol, changePySol, pytimeSpent, changePyTimeSpent}}>
       { children }
     </ResultContext.Provider>
   )
