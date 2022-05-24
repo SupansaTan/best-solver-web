f2b5250858d5f069427cd7ebdf362a5da3605a58
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-05-24 23:24:47 +0700

    update equation and method of root finding

--- src/wasm/c/findingRoot.c
+++ src/wasm/c/findingRoot.c
@@ -23,19 +23,19 @@ double f(double x)
       return pow(x,2) - 2;
       break;
     case 6:
-      return exp(pow(-x,2)) - x;
+      return exp(-pow(x,2)) - x;
       break;
     case 7:
       return -x + 1;
       break;
     case 8:
-      return pow(1/5,x) - 2;
+      return pow(0.2,x) - 2;
       break;
     case 9:
-      return pow(x,3) - 3*pow(x,2) - 4*x + 5;
+      return pow(x,3) - 3*(pow(x,2)) - 4*x + 5;
       break;
     case 10:
-      return - pow((1/3),x) + 2;
+      return - pow(0.3333,x) + 2;
       break;
     default:
       return -x;
@@ -63,19 +63,19 @@ double df(double x)
       return 2*x;
       break;
     case 6:
-      return exp(pow(x,2))*(2*x) - 1;
+      return -2 * exp(-pow(x,2))*x - 1;
       break;
     case 7:
       return -1;
       break;
     case 8:
-      return -log(5) * pow(1/5,x);
+      return -log(5) * pow(0.2,x);
       break;
     case 9:
       return 3*pow(x,2) - 6*x - 4;
       break;
     case 10:
-      return log(3) * pow((1/3),x);
+      return log(3) * pow(0.3333,x);
       break;
     default:
       return -x;
@@ -95,7 +95,7 @@ double bisection(float a, float b, int step)
 
   an = a;
   bn = b;
-  for(i=0; i<(step+1); i++)
+  for(i=0; i<step; i++)
   {
     mn = (an+bn)/2;
     fmn = f(mn);
@@ -139,7 +139,7 @@ double newton(float x0, float e, int maxIter)
   double xn, fxn, df_xn;
 
   xn = x0;
-  for(i=0; i<(maxIter+1); i++)
+  for(i=0; i<maxIter; i++)
   {
     fxn = f(xn);
     if(fabs(fxn) < e)
@@ -164,7 +164,7 @@ double findNewton(int num)
   selectFunc = num;
 
   double result;
-  result = newton(-0.7, 5, 16);
+  result = newton(-0.7, 2, 16);
 
   return result;
 }
@@ -181,7 +181,7 @@ double secant(float a, float b, int step)
 
   an = a;
   bn = b;
-  for(i=0; i<(step+1); i++)
+  for(i=0; i<step; i++)
   {
     mn = an - f(an)*(bn-an)/(f(bn)-f(an));
     fmn = f(mn);
@@ -222,7 +222,7 @@ double findSecant(int num)
 double regula(float a, float b, int maxIter)
 {
   int i;
-  double an, bn, mn, fmn;
+  double an, bn, mn;
 
   if(f(a)*f(b) >=  0)
   {
@@ -231,29 +231,24 @@ double regula(float a, float b, int maxIter)
 
   an = a;
   bn = b;
-  for(i=0; i<(maxIter+1); i++)
+  for(i=0; i<maxIter; i++)
   {
-    mn = (an*f(bn)-b*f(an)) / (f(bn)-f(an));
-    fmn = f(mn);
+    mn = an - (bn-an) * f(an)/(f(bn) - f(an));
 
-    if(f(an)*fmn < 0)
+    if(f(an)*f(mn) < 0)
     {
       bn = mn;
     }
-    else if(f(bn)*fmn < 0)
+    else if(f(bn)*f(mn) < 0)
     {
       an = mn;
     }
-    else if(fmn == 0)
+    else if(f(mn) == 0)
     {
       return mn;  // found exact solution
     }
-    else
-    {
-      return -1;
-    }
   }
-  return (an*f(bn)-b*f(an)) / (f(bn)-f(an));
+  return an - (bn-an) * f(an)/(f(bn) - f(an));
 }
 
 EMSCRIPTEN_KEEPALIVE
